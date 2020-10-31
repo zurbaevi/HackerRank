2d9d624ec526d84e3970858c7fbbeed046b9a12c
Nika <zurbaevi.nika@gmail.com>
2020-11-01 03:11:27 +0400

    Added solutions

---
+++ src/main/java/com/hackerrank/Java/WelcomeToJava.java
@@ -0,0 +1,11 @@
+package com.hackerrank.Java;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
