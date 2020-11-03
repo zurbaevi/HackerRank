f8ab1ef9009b233bd61051e16c0061269985ebd1
Nika <zurbaevi.nika@gmail.com>
2020-11-03 15:56:50 +0400

    Fix minor lacks

--- src/main/java/com/hackerrank/java/strings/JavaStringsIntroduction.java
+++ src/main/java/com/hackerrank/java/strings/JavaStringsIntroduction.java
@@ -11,8 +11,27 @@ public class JavaStringsIntroduction {
         String A = sc.next();
         String B = sc.next();
         System.out.println(A.length() + B.length());
-        System.out.println(A.length() > B.length() ? "No" : "Yes");
+        String str = "";
+        for (int i = 0; i < Math.min(A.length(), B.length()); i++) {
+            if ((int) (A.toLowerCase()).charAt(i) > (int) (B.toLowerCase()).charAt(i)) {
+                str = "Yes";
+                break;
+            } else if ((A.toLowerCase()).charAt(i) < (B.toLowerCase()).charAt(i)) {
+                str = "No";
+                break;
+            }
+        }
+        if (str.equals("")) {
+            if (A.length() > B.length()) {
+                str = "Yes";
+            } else {
+                str = "No";
+            }
+        }
+        System.out.println(str);
+
         System.out.println((A.toUpperCase()).charAt(0) + A.substring(1, A.length()) + " " + (B.toUpperCase()).charAt(0)
                 + B.substring(1, B.length()));
+        sc.close();
     }
 }
