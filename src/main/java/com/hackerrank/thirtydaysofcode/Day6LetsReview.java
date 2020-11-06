e9d42733511d730bc55de76b986c71a2e0d66442
Nika <zurbaevi.nika@gmail.com>
2020-11-06 21:33:24 +0400

    Added solutions

---
+++ src/main/java/com/hackerrank/thirtydaysofcode/Day6LetsReview.java
@@ -0,0 +1,28 @@
+package com.hackerrank.thirtydaysofcode;
+
+import java.util.Scanner;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class Day6LetsReview {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = "", str2 = "";
+        int noOf = sc.nextInt();
+        for (int i = 0; i < noOf; i++) {
+            String input = sc.next();
+            for (int j = 0; j < input.length(); j++) {
+                if (j % 2 == 0) {
+                    str1 = str1 + input.charAt(j);
+                } else {
+                    str2 = str2 + input.charAt(j);
+
+                }
+            }
+            System.out.println(str1 + " " + str2);
+            str1 = str2 = "";
+        }
+        sc.close();
+    }
+}
