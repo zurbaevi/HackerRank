7b11c7876fdeffe2e8ea887b614217e9ff84689d
Nika <zurbaevi.nika@gmail.com>
2020-11-10 22:52:40 +0400

    Added solutions

---
+++ src/main/java/com/hackerrank/thirtydaysofcode/Day10BinaryNumbers.java
@@ -0,0 +1,33 @@
+package com.hackerrank.thirtydaysofcode;
+
+import java.util.Scanner;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class Day10BinaryNumbers {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int r = n, counter = 0, maxOne = 0;
+        String s = "";
+        while (n > 0) {
+            r = n % 2;
+            if (r == 1) {
+                counter++;
+                if (counter > maxOne) {
+                    maxOne = counter;
+                }
+            } else {
+                counter = 0;
+            }
+            s = r + s;
+            n = n / 2;
+
+        }
+        System.out.println(maxOne);
+        scanner.close();
+    }
+}
