a2d2c9a24d84147c68e79d81ff1a2e586d7e07d6
Nika <zurbaevi.nika@gmail.com>
2020-11-12 21:17:32 +0400

    Added solutions

---
+++ src/main/java/com/hackerrank/thirtydaysofcode/Day112DArrays.java
@@ -0,0 +1,36 @@
+package com.hackerrank.thirtydaysofcode;
+
+import java.util.Scanner;
+
+public class Day112DArrays {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        int sum = 0, maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if ((i + 2) < 6 && (j + 2) < 6) {
+                    sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j]
+                            + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                }
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+            }
+
+        }
+        System.out.println(maxSum);
+        scanner.close();
+    }
+}
