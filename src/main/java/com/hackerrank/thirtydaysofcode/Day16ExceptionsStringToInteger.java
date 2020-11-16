937ab5e9deaf1579ebd1ac7bec0f97c0a947c771
Nika <zurbaevi.nika@gmail.com>
2020-11-17 02:23:17 +0400

    Added solutions

---
+++ src/main/java/com/hackerrank/thirtydaysofcode/Day16ExceptionsStringToInteger.java
@@ -0,0 +1,18 @@
+package com.hackerrank.thirtydaysofcode;
+
+import java.util.Scanner;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class Day16ExceptionsStringToInteger {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try {
+            System.out.println(Integer.parseInt(S));
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+    }
+}
