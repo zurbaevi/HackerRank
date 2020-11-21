35eacd01fa3c3b55711b4644c4684343615d9f5b
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-11-22 01:05:25 +0400

    Added solution

---
+++ src/main/java/com/hackerrank/thirtydaysofcode/Day21Generics.java
@@ -0,0 +1,37 @@
+package com.hackerrank.thirtydaysofcode;
+
+import java.util.Scanner;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class Day21Generics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+        Printer<Integer> intPrinter = new Printer<>();
+        Printer<String> stringPrinter = new Printer<>();
+        intPrinter.printArray(intArray);
+        stringPrinter.printArray(stringArray);
+        if (Printer.class.getDeclaredMethods().length > 1) {
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    }
+}
+
+class Printer<T> {
+    void printArray(T[] inputArray) {
+        for (T t : inputArray) {
+            System.out.println(t);
+        }
+    }
+}
