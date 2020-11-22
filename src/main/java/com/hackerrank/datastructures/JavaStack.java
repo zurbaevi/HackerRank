71e14dc90482b30e09a1474896a39149fd23d3ab
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-11-22 14:04:13 +0400

    Added solution

---
+++ src/main/java/com/hackerrank/datastructures/JavaStack.java
@@ -0,0 +1,40 @@
+package com.hackerrank.datastructures;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class JavaStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            Stack<Character> stack = new Stack<>();
+            String line = scanner.nextLine();
+            for (char c : line.toCharArray()) {
+                if (c == '{' || c == '(' || c == '[') {
+                    stack.push(c);
+                    continue;
+                }
+                if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                    stack.pop();
+                    continue;
+                }
+                if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                    continue;
+                }
+                if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                    stack.pop();
+                    continue;
+                }
+                if (c == '}' || c == ')' || c == ']') {
+                    stack.push(c);
+                    break;
+                }
+            }
+            System.out.println(stack.isEmpty());
+        }
+    }
+}
