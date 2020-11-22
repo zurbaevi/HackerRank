50f5698192eefaad382600afd4cbf741c683d958
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-11-22 13:59:37 +0400

    Added solution

---
+++ src/main/java/com/hackerrank/datastructures/JavaDequeue.java
@@ -0,0 +1,34 @@
+package com.hackerrank.datastructures;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class JavaDequeue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Deque<Integer> dq = new ArrayDeque<>();
+        HashSet<Integer> s = new HashSet<>();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int tmp = sc.nextInt();
+            dq.add(tmp);
+            s.add(tmp);
+            if (dq.size() == m) {
+                max = Math.max(s.size(), max);
+                int item = dq.remove();
+                if (!dq.contains(item)) {
+                    s.remove(item);
+                }
+            }
+        }
+        System.out.println(max);
+        sc.close();
+    }
+}
