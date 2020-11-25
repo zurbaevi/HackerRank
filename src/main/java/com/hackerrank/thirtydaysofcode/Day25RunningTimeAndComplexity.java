5f3f77a65426842a5cab6d55c72944d88e4c8d30
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-11-26 00:50:31 +0400

    Added solution

---
+++ src/main/java/com/hackerrank/thirtydaysofcode/Day25RunningTimeAndComplexity.java
@@ -0,0 +1,30 @@
+package com.hackerrank.thirtydaysofcode;
+
+import java.util.Scanner;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class Day25RunningTimeAndComplexity {
+    private static boolean isPrime(int number) {
+        if (number == 2) return true;
+        if (number < 2 || number % 2 == 0) return false;
+        for (int i = 3; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            if (isPrime(scanner.nextInt())) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not prime");
+            }
+        }
+        scanner.close();
+    }
+}
