8e46c2e7c3ed092b896ea0ad2a7f1a7a58899201
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-11-29 00:43:22 +0400

    Added solution

---
+++ src/main/java/com/hackerrank/java/oop/JavaAbstractClass.java
@@ -0,0 +1,35 @@
+package com.hackerrank.java.oop;
+
+import java.util.Scanner;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class JavaAbstractClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        MyBook new_novel = new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: " + new_novel.getTitle());
+        sc.close();
+
+    }
+
+    abstract static class Book {
+        String title;
+
+        String getTitle() {
+            return title;
+        }
+
+        abstract void setTitle(String s);
+    }
+
+    static class MyBook extends Book {
+        @Override
+        void setTitle(String s) {
+            super.title = s;
+        }
+    }
+}
