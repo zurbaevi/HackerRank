fc7d4be55975e49b1c092850799b751b708a9135
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-11-29 15:22:10 +0400

    Added solution

---
+++ src/main/java/com/hackerrank/java/oop/JavaInterface.java
@@ -0,0 +1,44 @@
+package com.hackerrank.java.oop;
+
+import java.util.Scanner;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class JavaInterface {
+
+    public static void main(String[] args) {
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+        sc.close();
+    }
+
+    static void ImplementedInterfaceNames(Object o) {
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++) {
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+
+    interface AdvancedArithmetic {
+        int divisor_sum(int n);
+    }
+
+    static class MyCalculator implements AdvancedArithmetic {
+        public int divisor_sum(int n) {
+            int sum = 0, i = 1;
+            while (n != 0 && i <= n) {
+                if (n % i == 0) {
+                    sum += i;
+                }
+                i++;
+            }
+            return sum;
+        }
+    }
+}
