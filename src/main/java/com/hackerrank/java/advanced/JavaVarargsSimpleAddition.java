3d371c2f1bd832cb06253acb67ad99c58e111d05
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-11-30 13:36:13 +0400

    Added solution

---
+++ src/main/java/com/hackerrank/java/advanced/JavaVarargsSimpleAddition.java
@@ -0,0 +1,58 @@
+package com.hackerrank.java.advanced;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class JavaVarargsSimpleAddition {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            int n6 = Integer.parseInt(br.readLine());
+            Add ob = new Add();
+            ob.add(n1, n2);
+            ob.add(n1, n2, n3);
+            ob.add(n1, n2, n3, n4, n5);
+            ob.add(n1, n2, n3, n4, n5, n6);
+            Method[] methods = Add.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class Add {
+        public void add(int... a) {
+            StringBuilder b = new StringBuilder();
+            int c = 0;
+            for (int i : a) {
+                b.append(i).append("+");
+                c += i;
+            }
+            System.out.print(b.substring(0, b.length() - 1));
+            System.out.println("=" + c);
+        }
+    }
+}
